9a9e53954b23152a21b33b7a5a7e874d79269c73
RossK <kelobang.opatile@gmail.com>
2020-07-15 19:44:36 +0200

    Empty Classes created

---
+++ Unit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StragtegyGame
+{
+    class Unit
+    {
+        protected int x, y, health, speed, attackRange;
+        protected String faction, symbol;
+        protected bool attack;
+
+        public Unit()
+        {
+
+        }
+
+        public Unit(int x, int y, int health, int speed, bool attack, int attackRange, String faction, String symbol)
+        {
+            this.x = x;
+            this.y = y;
+            this.health = health;
+            this.speed = speed;
+            this.attack = attack;
+            this.attackRange = attackRange;
+            this.faction = faction;
+            this.symbol = symbol;
+        }
+    }
+}
