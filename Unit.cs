b603535459f148ad6e9690a9e991cf99927dae3f
RossK <kelobang.opatile@gmail.com>
2020-07-17 19:00:17 +0200

    Classes modifed

--- Unit.cs
+++ Unit.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace StragtegyGame
 {
     class Unit
     {
-        protected int x, y, health, speed, attackRange;
-        protected String faction, symbol;
+        protected int attackRange;
+        protected String symbol;
         protected bool attack;
+        protected int x;
+        protected int y;
+        protected int health;
+        protected int speed;
+        protected String faction;
 
         public Unit()
         {
@@ -27,6 +33,61 @@ namespace StragtegyGame
             this.attackRange = attackRange;
             this.faction = faction;
             this.symbol = symbol;
+        }
+
+        ~Unit()
+        {
+
+        }
+
+        public int getX(int x)
+        {
+            return this.x;
+        }
+
+        public int getY()
+        {
+            return this.y;
+        }
+
+        public int getHealth()
+        {
+            return this.health;
+        }
+
+        public int getSpeed()
+        {
+            return this.speed;
+        }
+
+        public bool getAttack()
+        {
+            return this.attack;
+        }
+
+        public int getAttackRange()
+        {
+            return this.attackRange;
+        }
+
+        public string getFaction()
+        {
+            return this.faction;
+        }
+
+        public string getSymbol()
+        {
+            return this.symbol;
+        }
+
+        private void setX(int x)
+        {
+
+        }
+
+        private void setY(int y)
+        {
+
         }
     }
 }
